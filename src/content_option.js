53bfb8ba5b6c3eaa12b6296a50c1eb2a022846dd
Kiran Majid <87384847+Kironian@users.noreply.github.com>
2023-03-05 20:40:47 -0700

    v1

---
+++ src/content_option.js
@@ -0,0 +1,156 @@
+
+const logoimage = "Nate The Great";
+const logotext = "NATE THE GREAT";
+const meta = {
+    title: "Nate the Great",
+    description: "I’m Nate the Great, Master of Magic",
+};
+
+const introdata = {
+    title: "I am Nate the Great",
+    animated: {
+        first: "Welcome to my World of Magic",
+        second: "Be prepared to be Amazed by the Wonders of Illusion",
+        third: "Step into a World of Enchantment and Wonder with Our Master Magician",
+    },
+    description: "Unleash Your Imagination with Nate the Great's Mesmerizing Magic. Reach out to schedule a performance today!",
+    your_img_url: "https://iili.io/HWG6dRj.png",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "I'm Nate the Great, a master magician with over a decade of experience enchanting audiences with my mind-bending illusions and captivating performances. My shows are a perfect blend of classic and modern magic, leaving the audience awestruck and spellbound. My exceptional showmanship and magnetic personality have earned me a loyal fan following, and my unparalleled dedication to my craft has made me one of the most sought-after magicians in the industry. With my unique style and imaginative approach to magic, I'm sure to leave you wondering how I did it!",
+};
+const worktimeline = [{
+
+        jobtitle: "The Restaurant Magic Show",
+        where: "Portland, OR",
+        date: "3/5/23",
+    },
+    {
+        jobtitle: "The Aquarium Magic Show",
+        where: "Los Angeles, CA",
+        date: "3/13/23",
+    },
+    {
+        jobtitle: "The Venue Show",
+        where: "Denver, CO",
+        date: "3/20/23",
+    },
+];
+
+const skills = [{
+        name: "Showmanship",
+        value: 90,
+    },
+    {
+        name: "Storytelling",
+        value: 85,
+    },
+    {
+        name: "Humour",
+        value: 80,
+    },
+    {
+        name: "Adaptability",
+        value: 80,
+    },
+    {
+        name: "Promptness",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Children Shows",
+        description: "Nate the Great offers fun and interactive children's shows that will leave kids amazed and entertained. With his impressive magic tricks and engaging personality, he'll keep the children captivated from start to finish.",
+    },
+    {
+        title: "Private Parties",
+        description: "Looking for unique and memorable entertainment for your private party? Nate the Great has got you covered. From intimate gatherings to large celebrations, he'll provide a customized performance that fits your specific needs and preferences.",
+    },
+    {
+        title: "Corporate Events",
+        description: "Nate the Great brings his magic to the corporate world, offering tailored performances that can help promote your brand and leave a lasting impression on clients and employees alike. He can perform during product launches, conferences, trade shows, and other corporate events.",
+    },
+    {
+        title: "Magic Workshops and Lectures",
+        description: "For those interested in learning the secrets behind Nate the Great's magic, he offers workshops and lectures that will teach you the techniques and principles of magic. Whether you're a beginner or an experienced magician, Nate will help you hone your skills and improve your performances.",
+    },
+    {
+        title: "TV & Film",
+        description: "Nate the Great is available for TV and film projects, offering his expertise and experience to help bring your project to life. Whether you need a consultant, adviser, or performer, Nate can provide the magic touch that will make your project stand out.",
+    },
+];
+
+const dataportfolio = [{
+    img: "https://i.ytimg.com/vi/EAN-PwRfJcA/maxresdefault.jpg",
+     video: "https://www.youtube.com/watch?v=EAN-PwRfJcA",
+     description: "Watch my latest magic show",
+        link: "#",
+    },
+    {
+       
+    img: "https://i.ytimg.com/vi/EAN-PwRfJcA/maxresdefault.jpg",
+    video: "https://www.youtube.com/watch?v=EAN-PwRfJcA",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        
+    img: "https://i.ytimg.com/vi/EAN-PwRfJcA/maxresdefault.jpg",
+    video: "https://www.youtube.com/watch?v=EAN-PwRfJcA",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        
+    img: "https://i.ytimg.com/vi/EAN-PwRfJcA/maxresdefault.jpg",
+    video: "https://www.youtube.com/watch?v=EAN-PwRfJcA",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+       
+    img: "https://i.ytimg.com/vi/EAN-PwRfJcA/maxresdefault.jpg",
+    video: "https://www.youtube.com/watch?v=EAN-PwRfJcA",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        
+    img: "https://i.ytimg.com/vi/EAN-PwRfJcA/maxresdefault.jpg",
+    video: "https://www.youtube.com/watch?v=EAN-PwRfJcA",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    }
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "nate@natethegreat.com",
+    YOUR_FONE: "(805) 722-7070",
+    description: "Ready to be amazed? Contact Nate the Great to schedule a magical performance that will leave you and your guests spellbound. Whether you're planning a children's party, private event, or corporate function, Nate has the experience and expertise to make it an unforgettable experience. Get in touch today to book your date and experience the magic!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    facebook: "https://facebook.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+    logoimage,
+};
---
+++ src/header/index.js
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import { VscGrabber, VscClose } from "react-icons/vsc";
+import { Link } from "react-router-dom";
+import { logotext, logoimage, socialprofils } from "../content_option";
+import Themetoggle from "../components/themetoggle";
+
+const Headermain = () => {
+  const [isActive, setActive] = useState(false);
+  const [isDesktop, setDesktop] = useState(window.innerWidth >= 768);
+
+  const handleToggle = () => {
+    setActive(prevActive => !prevActive);
+    document.body.classList.toggle("ovhidden");
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setDesktop(window.innerWidth >= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <>
+      <header className="fixed-top site__header">
+        <div className="d-flex align-items-center justify-content-between">
+          <Link className="navbar-brand nav_ac" to="/">
+            {logoimage}
+          </Link>
+          <div className="d-flex align-items-center">
+            <Themetoggle />
+            {isDesktop ? (
+              <ul className="desktop__menu d-none d-md-flex">
+                <li className="menu_item">
+                  <Link to="/" className="my-3">
+                    Home
+                  </Link>
+                </li>
+                <li className="menu_item">
+                  <Link to="/portfolio" className="my-3">
+                    Portfolio
+                  </Link>
+                </li>
+                <li className="menu_item">
+                  <Link to="/about" className="my-3">
+                    About
+                  </Link>
+                </li>
+                <li className="menu_item">
+                  <Link to="/contact" className="my-3">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            ) : (
+              <button
+                className="menu__button nav_ac d-md-none"
+                onClick={handleToggle}
+              >
+                {!isActive ? <VscGrabber /> : <VscClose />}
+              </button>
+            )}
+          </div>
+        </div>
+
+        <div
+          className={`site__navigation ${
+            isActive ? "menu__opend" : ""
+          }`}
+        >
+          <button className="close__button" onClick={handleToggle}>
+            <VscClose />
+          </button>
+          <div className="bg__menu h-100">
+            <div className="menu__wrapper">
+              <div className="menu__container p-3">
+                <ul className="the_menu">
+                  <li className="menu_item">
+                    <Link onClick={handleToggle} to="/" className="my-3">
+                      Home
+                    </Link>
+                  </li>
+                  <li className="menu_item">
+                    <Link
+                      onClick={handleToggle}
+                      to="/portfolio"
+                      className="my-3"
+                    >
+                      Portfolio
+                    </Link>
+                  </li>
+                  <li className="menu_item">
+                    <Link onClick={handleToggle} to="/about" className="my-3">
+                      About
+                    </Link>
+                  </li>
+                  <li className="menu_item">
+                    <Link
+                      onClick={handleToggle}
+                      to="/contact"
+                      className="my-3"
+                    >
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="menu_footer d-flex flex-column flex">
+            <div className="d-flex">
+              <a href={socialprofils.facebook}>Facebook</a>
+              <a href={socialprofils.github}>Github</a>
+              <a href={socialprofils.twitter}>Twitter</a>
+            </div>
+            <p className="copyright m-0">
+              copyright __ {logotext}
+            </p>
+          </div>
+        </div>
+      </header>
+      <div className="br-top"></div>
+      <div className="br-bottom"></div>
+      <div className="br-left"></div>
+      <div className="br-right"></div>
+    </>
+  );
+};
+
+export default Headermain;
+
